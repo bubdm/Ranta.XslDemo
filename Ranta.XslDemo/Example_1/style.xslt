9ef8c5aa06955ec05af1009327c7283abdd57b12
HarrisZhang <hc_zhang@Ctrip.com>
2016-09-14 12:24:39 +0800

    update

---
+++ Ranta.XslDemo/Example_1/style.xslt
@@ -0,0 +1,29 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+  <xsl:output method="xml" indent="yes"/>
+
+  <xsl:template match="/">
+    <xsl:apply-templates />
+  </xsl:template>
+
+  <xsl:template match="student">
+    <h2>
+      <xsl:apply-templates />
+    </h2>
+  </xsl:template>
+
+  <xsl:template match="teacher">
+    <h3>
+      <xsl:apply-templates />
+    </h3>
+  </xsl:template>
+
+  <xsl:template match="age">
+    <age>
+      <xsl:apply-templates />
+    </age>
+  </xsl:template>
+
+</xsl:stylesheet>
--- Ranta.XslDemo/Program.cs
+++ Ranta.XslDemo/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Xsl;
 
 namespace Ranta.XslDemo
 {
@@ -10,6 +12,29 @@ namespace Ranta.XslDemo
     {
         static void Main(string[] args)
         {
+            Example_1();
+        }
+
+        static void Example_1()
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+
+            transform.Load("Example_1\\style.xslt");
+
+            StringBuilder output = new StringBuilder();
+
+            using (StringWriter writer = new StringWriter(output))
+            {
+                transform.Transform("Example_1\\data.xml", new XsltArgumentList { }, writer);
+            }
+
+            Console.WriteLine(output.ToString());
+
+            string text = output.ToString();
+
+            Console.WriteLine(".......................");
+
+            Console.Read();
         }
     }
 }
